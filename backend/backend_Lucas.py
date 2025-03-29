73b63d0cc53b9d170d12253264f7f3191142f012
DusseauL <dusseaul55@gmail.com>
2025-03-29 14:50:45 -0400

    Flask Test 2

--- backend/backend_Lucas.py
+++ backend/backend_Lucas.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Niko Roberts'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name='Niko'):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'You submitted: {name}'
 
 if __name__ == '__main__':
     app.run(debug=True)
