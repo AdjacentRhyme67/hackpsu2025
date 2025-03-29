caa323a6919639ad4d799be8524d7214890c0cf5
DusseauL <dusseaul55@gmail.com>
2025-03-29 14:33:46 -0400

    Flask works!

--- backend/backend_Lucas.py
+++ backend/backend_Lucas.py
@@ -1,10 +1,10 @@
-from flask import Flaskgit
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Niko Roberts'
 
 if __name__ == '__main__':
     app.run(debug=True)
